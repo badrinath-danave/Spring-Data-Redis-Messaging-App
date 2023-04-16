cf1604a741d8915e104d937ef6ea28e203ad1512
Badrinath Danave <badrinathdanave@gmail.com>
2023-04-17 00:16:53 +0530

    added dependencies for project

---
+++ src/test/java/com/springboot/redis/messaging/app/SpringDataRedisMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.redis.messaging.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisMessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
